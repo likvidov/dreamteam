0235e8f9d545a8a405dce4c7052e4eb0d4bb8a94
likvidov <likvidya@gmail.com>
2020-06-01 15:23:21 +0300

    new preloader

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 window.onload = function () {
+  var main = document.querySelector('main');
   var preloader = document.querySelector('.preloader');
   this.setTimeout(() => {
     preloader.remove();
+    main.classList = '';
   }, 1500);
 }
